7ebbb6eb6603c1bed058e1cf670cc2138aad883b
viralsavani <viralsavani@gmail.com>
2014-12-13 00:24:45 -0800

    Macro Simple

--- exam/src/exam/core.clj
+++ exam/src/exam/core.clj
@@ -1,6 +1,17 @@
 (ns exam.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;Simple example for finals preparation
+
+(defmacro unless [expr form]
+  (list 'if expr nil form))
+
+;For chain execution
+(defmacro chain
+  ([x form] (list '. x form))
+  ([x form & more] (concat (list 'chain (list '. x form)) more)))
+
+;Simple AND macro: stops at first nil or false
+(and (+ 1 2) (inc 12) (first [12 2 3]) (- 1 1) false (+ 10 11))
+
+;Simple OR macro: stops at first true
+(or nil false (inc 12))              ; Exececutes unitll it finds first true or some function returning value.
